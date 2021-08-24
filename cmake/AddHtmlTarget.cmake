e64318e756934003c7b47e6042013fabac3a1229
TANIGUCHI Yasuaki <yasuakit@gmail.com>
2021-08-24 17:35:02 +0900

    (2) cmake related   * The stylesheet/ was removed from distfiles in CMakeList.txt.   * Usages comment of function was added at the top of the files.   * AddGncDocTargets.cmake:      - SVG files were removed from the list "${figure}" because they aren't used right now.      - "${dtd_files}" was added to support multiple dtd files.   * AddChmTarget.cmake:      - MakeChm.cmake was merged.      - The working directory was changed to "${CMAKE_CURRENT_BINARY_DIR}/htmlhelp".      - The methods of copying figures and icons were changed to use add_custom_command.      - Dependencies on "${dtd_files}" were added.      - The dependencies were rearranged.      - Unused functionalities in GnuCash, such as alias and map, were removed.      - The "--stringparam gnc.lang ${lang}" argument is added to the xsltproc so as to swich the character codes.      - The dtd file name for xsltproc was changed to support the new xsl/ structure.    * AddEpubTarget.cmake:      - The dtd file name for xsltproc was changed to support the new xsl/ structure.      - Dependencies on "${dtd_files}" were added.      - The admonitions and Gnucash icons were added.    * AddGHelpTarget.cmake:      - The dtd file name for xsltproc was changed to support the new xsl/ structure.      - Dependencies on "${dtd_files}" were added.    * AddHtmlTarget.cmake:      - The dtd file name for xsltproc was changed to support the new xsl/ structure.      - Dependencies on "${dtd_files}" were added.      - The admonitions and Gnucash icons were added.    * AddPdfTarget.cmake:      - The working directory was changed to "${CMAKE_CURRENT_BINARY_DIR}".      - fop.xconf.in and guide/ja/CMakeList.txt were changed to support the new working directory.      - The dtd file name for xsltproc was changed to support the new xsl/ structure.      - Dependencies on "${dtd_files}" were added.      - The admonitions and Gnucash icons were added.    * MakeChm.cmake was removed.

--- cmake/AddHtmlTarget.cmake
+++ cmake/AddHtmlTarget.cmake
@@ -1,56 +1,60 @@
 #
-# Functions to generate html documentation
+# A function to generate html documentation
 #
-# Paremeters:
-# - docname: basename of the main xml file. Will be used to locate
-#            this primary xml file and for various output files/directories
-# - lang: language of the current document
-# - entities: list of all xml files this document is composed of
-# - figdir: name of the directory holding the images
+# FUNCTION:
+#   add_html_target
+# ARGUMENTS:
+# - docname: The basename of the main xml file. Will be used to locate
+#            this primary xml file and for various output files/directories.
+#            Either "gnucash-guide" or "gnucash-help" now.
+# - lang: The language of the current document, such as "C", "de" and so on.
+# - entities: A list of all xml files this document is composed of. ONLY filename, WITHOUT path.
+#             It does NOT contain "${docname}.xml".
+# - figures: A list of FULL PATH image files.
+# - dtd_files: A list of FULL PATH DTD files.
 
-function (add_html_target docname lang entities figures)
 
-    set(styledir "${CMAKE_SOURCE_DIR}/stylesheet")
-    file(GLOB styleicons "${CMAKE_SOURCE_DIR}/stylesheet/*.png")
+function (add_html_target docname lang entities figures dtd_files)
 
+    # Setup base directory
+    # We don't need to make work directory, we can output HTML files directly.
     set(BUILD_DIR "${DOCDIR_BUILD}/${lang}/${docname}")
     file(MAKE_DIRECTORY "${BUILD_DIR}")
-    file(MAKE_DIRECTORY "${BUILD_DIR}/figures")
-    file(MAKE_DIRECTORY "${BUILD_DIR}/stylesheet")
 
     # Convert xml to html with xsltproc
-    # xsltproc --xinclude -o outputdir/ /usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl filename.xml
+    # xsltproc --xinclude -o outputdir/ stylesheet.xsl "${docname}.xml"
     add_custom_command(
-        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/htmltrigger"
+        OUTPUT "${BUILD_DIR}/index.html"
         COMMAND  ${XSLTPROC} ${XSLTPROCFLAGS} ${XSLTPROCFLAGS_HTML}
                              -o "${BUILD_DIR}/"
-                             --param use.id.as.filename "1"
-                             --stringparam chunker.output.encoding UTF-8
-                             "${CMAKE_SOURCE_DIR}/xsl/general-customization.xsl"
+                             "${CMAKE_SOURCE_DIR}/xsl/gnc-custom-html.xsl"
                              "${CMAKE_CURRENT_SOURCE_DIR}/${docname}.xml"
-        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/htmltrigger"
-        DEPENDS ${entities} "${docname}.xml" "${CMAKE_SOURCE_DIR}/docbook/gnc-docbookx.dtd")
+        DEPENDS ${entities} "${docname}.xml" "${dtd_files}")
+
 
     # Copy figures for this document
     file(MAKE_DIRECTORY "${BUILD_DIR}/figures")
     add_custom_command(
-        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/html_figtrigger"
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/htmlfigtrigger"
         COMMAND ${CMAKE_COMMAND} -E copy ${figures} "${BUILD_DIR}/figures"
-        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/html_figtrigger"
+        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/htmlfigtrigger"
         DEPENDS ${figures})
 
-    # Copy style icons for this document (warning, info,...)
-    file(MAKE_DIRECTORY "${BUILD_DIR}/stylesheet")
+    # Copy icon files for this documents
+    # Removed dependency because icon files are rarely changed. 
+    file(MAKE_DIRECTORY "${BUILD_DIR}/images/callouts")
     add_custom_command(
-        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/styletrigger"
-        COMMAND  ${CMAKE_COMMAND} -E copy ${styleicons} "${BUILD_DIR}/stylesheet"
-        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/styletrigger"
-        DEPENDS ${styleicons})
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/htmlicontrigger"
+        # PNG admonition and GnuCash icons are used in html.
+        COMMAND cp -f "${CMAKE_SOURCE_DIR}/xsl/images/*.png"  "${BUILD_DIR}/images"
+        # SVG callout icons are used in html.
+        COMMAND cp -f "${CMAKE_SOURCE_DIR}/xsl/images/callouts/*.svg"  "${BUILD_DIR}/images/callouts"
+        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/htmlicontrigger")
 
     add_custom_target("${lang}-${docname}-html"
-        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/htmltrigger"
-                "${CMAKE_CURRENT_BINARY_DIR}/html_figtrigger"
-                "${CMAKE_CURRENT_BINARY_DIR}/styletrigger")
+        DEPENDS "${BUILD_DIR}/index.html"
+                "${CMAKE_CURRENT_BINARY_DIR}/htmlfigtrigger"
+                "${CMAKE_CURRENT_BINARY_DIR}/htmlicontrigger")
 
     add_dependencies(${docname}-html "${lang}-${docname}-html")
 
--- cmake/AddPdfTarget.cmake
+++ cmake/AddPdfTarget.cmake
@@ -1,4 +1,18 @@
-function (add_pdf_target docname lang entities figures)
+#
+# A function to generate PDF documentation
+#
+# FUNCTION:
+#   add_pdf_target
+# ARGUMENTS:
+# - docname: The basename of the main xml file. Will be used to locate
+#            this primary xml file and for various output files/directories.
+#            Either "gnucash-guide" or "gnucash-help" now.
+# - lang: The language of the current document, such as "C", "de" and so on.
+# - entities: A list of all xml files this document is composed of. ONLY filename, WITHOUT path.
+#             It does NOT contain "${docname}.xml".
+# - figures: A list of FULL PATH image files.
+# - dtd_files: A list of FULL PATH DTD files.
+function (add_pdf_target docname lang entities figures dtd_files)
 
     set(fofile "${docname}.fo")
     set(pdffile "${docname}.pdf")
@@ -6,32 +20,22 @@ function (add_pdf_target docname lang entities figures)
     set(BUILD_DIR "${DOCDIR_BUILD}/${lang}")
     file(MAKE_DIRECTORY "${BUILD_DIR}")
 
-    # Determine paper format depending on language (which maps to the document's directory name)
-    # * for language "C" (fallback language) determine paper format based on current locale
-    # * all others use A4.
-    set (XSLTFLAGS_FO "--stringparam paper.type A4")
-    if (lang STREQUAL "C")
-        # For the fallback language determine paper format depending on locale
-        # Only US or C will be set to letter. All others use A4.
-        set (ENV_LANG $ENV{LANG})
-        if (ENV_LANG AND ENV_LANG MATCHES ".*_us.*|C") # Replacing ENV_LANG here with if ($ENV{LANG}) won't work.
-            set (XSLTFLAGS_FO "--stringparam paper.type letter")
-        endif()
-    endif()
 
     add_custom_command(
         OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${fofile}"
         COMMAND ${XSLTPROC} ${XSLTPROCFLAGS} ${XSLTPROCFLAGS_FO}
+                            --stringparam gnc.lang ${lang}
                             -o "${CMAKE_CURRENT_BINARY_DIR}/${fofile}"
-                            --stringparam fop1.extensions 1
-                            "${CMAKE_SOURCE_DIR}/xsl/1.79.2/fo/docbook.xsl"
+                            "${CMAKE_SOURCE_DIR}/xsl/gnc-custom-fo.xsl"
                             "${CMAKE_CURRENT_SOURCE_DIR}/${docname}.xml"
-        DEPENDS ${entities} "${docname}.xml" "${CMAKE_SOURCE_DIR}/docbook/gnc-docbookx.dtd")
+        DEPENDS ${entities} "${docname}.xml" ${dtd_files})
 
     configure_file("${FOP_XCONF}" "${CMAKE_CURRENT_BINARY_DIR}/fop.xconf")
 
     add_custom_command(
         OUTPUT "${BUILD_DIR}/${pdffile}"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_SOURCE_DIR}/xsl/images" "${CMAKE_CURRENT_BINARY_DIR}/images"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_SOURCE_DIR}/figures" "${CMAKE_CURRENT_BINARY_DIR}/figures"
         COMMAND ${FOP} ${FOPFLAGS}
                         -l ${lang}
                         -c "${CMAKE_CURRENT_BINARY_DIR}/fop.xconf"
