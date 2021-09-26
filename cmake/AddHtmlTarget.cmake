cc33497bb0c47b8df4a1fc1a36e28de90b98dd28
TANIGUCHI Yasuaki <yasuakit@gmail.com>
2021-09-26 19:03:29 +0900

    Modify CMake files for HTML to support new xsl/ structure.
    Caution: This commit leads to build failure.
    
    AddHtmlTarget.cmake:
      - Add GnuCash icons.
      - Add OASIS XSL admonitions.
      - Support multiple DTD files.
      - Change build directories are created when make html is run.
    
    ---
    cmake -S ../gnucash-docs -B . -DWITH_PDF=OFF -DWITH_EPUB=OFF
    cmake --build .
    cmake --install . --prefix ${prefix}
    
    Then ghelp and HTML will be built and installed successfully.
    Customization of XSL files is omitted. Therefore simple HTML files
    will be created.

--- cmake/AddHtmlTarget.cmake
+++ cmake/AddHtmlTarget.cmake
@@ -1,60 +1,87 @@
 #
-# Functions to generate html documentation
+# A function to generate html documentation
 #
-# Paremeters:
-# - docname: basename of the main xml file. Will be used to locate
-#            this primary xml file and for various output files/directories
-# - lang: language of the current document
-# - entities: list of all xml files this document is composed of
-# - figdir: name of the directory holding the images
+# FUNCTION:
+#   add_html_target
+# ARGUMENTS:
+# - docname: The basename of the main xml file. Will be used to locate
+#            this primary xml file and for various output files/directories.
+#            Either "gnucash-guide" or "gnucash-help" now.
+# - lang: The language of the current document, such as "C", "de" and so on.
+# - entities: A list of all xml files this document is composed of. ONLY filename, WITHOUT path.
+#             It does NOT contain "${docname}.xml".
+# - figures: A list of FULL PATH image files.
+# - dtd_files: A list of FULL PATH DTD files.
+#
+function (add_html_target docname lang entities figures dtd_files)
 
-function (add_html_target docname lang entities figures)
+    # Prepare variables and directories for build
+    set(fmt "html")
+    set(outfile "index.html")
 
-    set(styledir "${CMAKE_SOURCE_DIR}/stylesheet")
-    file(GLOB styleicons "${CMAKE_SOURCE_DIR}/stylesheet/*.png")
+    # We don't need to make work directory, we can output HTML files directly.
+    set(OUTPUT_DIR "${CMAKE_BINARY_DIR}/share/doc/${lang}/${docname}")
+    set(BUILD_DIR "${OUTPUT_DIR}")
 
-    set(BUILD_DIR "${DOCDIR_BUILD}/${lang}/${docname}")
-    file(MAKE_DIRECTORY "${BUILD_DIR}")
-    file(MAKE_DIRECTORY "${BUILD_DIR}/figures")
-    file(MAKE_DIRECTORY "${BUILD_DIR}/stylesheet")
+    # GnuCash-specific xsl files
+    file(GLOB xsl_files "${CMAKE_SOURCE_DIR}/xsl/*.xsl")
+    file(GLOB gnucash_icon_files "${CMAKE_SOURCE_DIR}/xsl/icons/*")
+
 
     # Convert xml to html with xsltproc
-    # xsltproc --xinclude -o outputdir/ /usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl filename.xml
     add_custom_command(
-        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/htmltrigger"
+        OUTPUT "${OUTPUT_DIR}/${outfile}"
         COMMAND  ${XSLTPROC} ${XSLTPROCFLAGS} ${XSLTPROCFLAGS_HTML}
-                             -o "${BUILD_DIR}/"
-                             --param use.id.as.filename "1"
-                             --stringparam chunker.output.encoding UTF-8
-                             "${CMAKE_SOURCE_DIR}/xsl/general-customization.xsl"
+                             -o "${OUTPUT_DIR}/"
+                             "${CMAKE_SOURCE_DIR}/xsl/gnc-custom-html.xsl"
                              "${CMAKE_CURRENT_SOURCE_DIR}/${docname}.xml"
-        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/htmltrigger"
-        DEPENDS ${entities} "${docname}.xml" "${CMAKE_SOURCE_DIR}/docbook/gnc-docbookx.dtd")
+        DEPENDS ${entities} "${docname}.xml" ${dtd_files} ${xsl_files}
+                "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-preparedir-trigger")
+
 
     # Copy figures for this document
-    file(MAKE_DIRECTORY "${BUILD_DIR}/figures")
     add_custom_command(
-        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/html_figtrigger"
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-fig-trigger"
         COMMAND ${CMAKE_COMMAND} -E copy ${figures} "${BUILD_DIR}/figures"
-        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/html_figtrigger"
-        DEPENDS ${figures})
+        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-fig-trigger"
+        DEPENDS ${figures}
+                "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-preparedir-trigger")
 
-    # Copy style icons for this document (warning, info,...)
-    file(MAKE_DIRECTORY "${BUILD_DIR}/stylesheet")
+    # Copy XSL icons
     add_custom_command(
-        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/styletrigger"
-        COMMAND  ${CMAKE_COMMAND} -E copy ${styleicons} "${BUILD_DIR}/stylesheet"
-        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/styletrigger"
-        DEPENDS ${styleicons})
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-xslticon-trigger"
+        # PNG admonition icons are used in html.
+        COMMAND cp -f "${CMAKE_SOURCE_DIR}/xsl/images/*.png" "${BUILD_DIR}/images"
+        # SVG callout icons are used in html.
+        COMMAND cp -f "${CMAKE_SOURCE_DIR}/xsl/images/callouts/*.svg" "${BUILD_DIR}/images/callouts"
+        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-xslticon-trigger"
+        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-preparedir-trigger")
 
-    add_custom_target("${lang}-${docname}-html"
-        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/htmltrigger"
-                "${CMAKE_CURRENT_BINARY_DIR}/html_figtrigger"
-                "${CMAKE_CURRENT_BINARY_DIR}/styletrigger")
+    # Copy GnuCash-Specific icons
+    add_custom_command(
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-gnucashicon-trigger"
+        COMMAND cp -f "${CMAKE_SOURCE_DIR}/xsl/icons/*" "${BUILD_DIR}/images"
+        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-gnucashicon-trigger"
+        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-xslticon-trigger"
+                ${gnucash_icon_files})
 
-    add_dependencies(${docname}-html "${lang}-${docname}-html")
+    # Prepare ${BUILD_DIR}
+    add_custom_command(
+        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-preparedir-trigger"
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${BUILD_DIR}/figures"
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${BUILD_DIR}/images/callouts"
+        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-preparedir-trigger")
 
-    install(DIRECTORY ${BUILD_DIR}
-      DESTINATION "${CMAKE_INSTALL_DOCDIR}/${lang}"
-      )
+    # TARGET dependencies
+    add_custom_target("${lang}-${docname}-${fmt}"
+        DEPENDS "${OUTPUT_DIR}/${outfile}"
+                "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-fig-trigger"
+                "${CMAKE_CURRENT_BINARY_DIR}/${fmt}-gnucashicon-trigger")
+
+    add_dependencies(${docname}-${fmt} "${lang}-${docname}-${fmt}")
+
+    install(DIRECTORY ${OUTPUT_DIR}
+      DESTINATION "share/doc/${lang}"
+      OPTIONAL)
+
 endfunction()
