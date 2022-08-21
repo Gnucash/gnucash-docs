1723d8a28a5c74694e7c7476eb151e08e3e8cf7c
Geert Janssens <geert@kobaltwit.be>
2022-08-21 22:23:26 +0200

    Fix build error introduced in most recent merge

--- cmake/AddXdgHelpTarget.cmake
+++ cmake/AddXdgHelpTarget.cmake
@@ -57,7 +57,8 @@ function (add_xdghelp_target docname lang entities figures)
         add_custom_command(
             OUTPUT ${dest_figures}
             COMMAND ${CMAKE_COMMAND} -E copy ${source_figures} "${BUILD_DIR}/figures"
-        DEPENDS ${source_figures} "${CMAKE_CURRENT_BINARY_DIR}/xdgghelptrigger")
+            DEPENDS ${source_figures} "${CMAKE_CURRENT_BINARY_DIR}/xdgghelptrigger")
+    endif()
 
     add_custom_target("${lang}-${docname}-xdghelp"
         DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/xdghelptrigger"
